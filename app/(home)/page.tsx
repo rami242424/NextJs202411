12c7e2fe37f19ca7314dbdbb0afc4fa535e6973c
Rami <138556019+rami242424@users.noreply.github.com>
2024-11-30 21:45:29 +0900

    loading & fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react"
+
 export const metadata = {
     title: 'Home'
 }
 
 export default function Page(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() =>{
+        const response = await fetch("http://nomad-movies.nomadcoders.workers.dev/movies");
+        const json = await response.json();
+        setMovies(json);
+        setIsLoading(false);
+    }
+    useEffect(() => {
+        getMovies();
+    }, []);
     return (
         <div>
-           <h1>Home</h1>
+           {isLoading ? "Loading..." : JSON.stringify(movies)}
         </div>
     )
 }
