e0ff3334797b401d4f7b35268ff3bf37f2c9f446
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-05 22:30:27 +0900

    edit

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,10 @@ export const metadata = {
 const URL = "http://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-    return fetch(URL).then(response => response.json());
+    // return fetch(URL).then(response => response.json());
+    const response = await fetch(URL);
+    const json = await response.json()
+    return json;
 }
 
 export default function Page() {
