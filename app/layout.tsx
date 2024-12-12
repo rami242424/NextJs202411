195845a4523aaf873a6d1855d4fa4ea1d6d4372e
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-12 21:49:53 +0900

    about-us layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-// import { Metadata } from "next";
-// import Navigation from "../components/navigation";
+import { Metadata } from "next";
+import Navigation from "../components/navigation";
 
-// export const metadata: Metadata = {
-//   title: {
-//     template: "%s | Next Movies",
-//     default: "Loading..."
-//   }
-// }
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Next Movies",
+    default: "Loading..."
+  }
+}
 
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <Navigation />
-//         {children}
-//       </body>
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        {children}
+      </body>
 
-//     </html>
-//   )
-// }
+    </html>
+  )
+}
