51f4d9268913952a995af64a344f2336ccb8ba42
Rami <138556019+rami242424@users.noreply.github.com>
2024-12-16 11:56:22 +0900

    navigation page

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,19 +1,19 @@
-// "use client"
+"use client"
 
-// import Link from "next/link";
-// import { usePathname } from "next/navigation";
-// import { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
-// export default function Navigation(){
-//     const path = usePathname();
-//     return (
-//         <nav>
-//             <ul>
-//                 <li>
-//                     <Link href='/'>Home</Link> {path === "/" ? "✔️" : ""}
-//                 </li>
-//                 <li><Link href="/about-us">About us</Link>{path === "/about-us" ? "✔️" : ""}</li>
-//             </ul>
-//         </nav>
-// )
-// }
+export default function Navigation(){
+    const path = usePathname();
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href='/'>Home</Link> {path === "/" ? "✔️" : ""}
+                </li>
+                <li><Link href="/about-us">About us</Link>{path === "/about-us" ? "✔️" : ""}</li>
+            </ul>
+        </nav>
+)
+}
